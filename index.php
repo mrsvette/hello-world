2f88917a22804fc7ceb65449fd063e9b86944c12
Farid Efendi <mrsvette69@gmail.com>
2016-08-30 17:31:16 +0700

    3rd commit on branch 2

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 //this comment added on branch2, with also edit once again
-<<<<<<< HEAD
 //this comment added on branch3
-=======
->>>>>>> 2ca42b4e19a8f451d1af41ed8fe91449d1ba3dd8
 $to = 'mrsvette@localhost.com'; // replace with your address here
 $subject = 'Test mail';
 $message = 'Hello! This is a simple test email message.';
