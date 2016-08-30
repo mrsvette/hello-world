232618ecbddd7832fa98de96bcf126ad7d62436c
Farid Efendi <mrsvette69@gmail.com>
2016-08-30 16:38:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$to = 'mrsvette@localhost.com'; // <– replace with your address here
+$subject = 'Test mail';
+$message = 'Hello! This is a simple test email message.';
+$from = 'root@localhost.com';
+$headers = 'From:' . $from;
+mail($to,$subject,$message,$headers);
+echo 'Mail Sent.';
